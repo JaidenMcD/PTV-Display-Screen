cd299444bc70af8dce79a76354e771f432b26a9b
JaidenMcD <jmcd0014@student.monash.edu>
2025-12-04 12:57:29 +1100

    Update run.py

--- run.py
+++ run.py
@@ -22,19 +22,13 @@ pygame.init()
 pygame.mouse.set_visible(False)
 
 if device == 1:
-    # --- Read actual framebuffer resolution ---
-    with open("/sys/class/graphics/fb0/virtual_size", "r") as f:
-        w, h = f.read().strip().split(",")
-        fb_w = int(w)
-        fb_h = int(h)
-    screen = pygame.display.set_mode((fb_w, fb_h))
+    screen = pygame.display.set_mode(SCREEN_RES, pygame.FULLSCREEN | pygame.NOFRAME)
 else:
     screen = pygame.display.set_mode(SCREEN_RES)
 
+info = pygame.display.Info()
+print("Pygame display info:", info.current_w, info.current_h)
 
-
-update_interval = 5 # seconds
-last_update = 0
 
 
 clock = pygame.time.Clock()
@@ -59,6 +53,9 @@ while running:
 
     # Time to next train Box
     pygame.draw.rect(screen, BLACK, (379, 15, 91, 33))
+
+
+    pygame.draw.rect(screen, SHOWGROUNDS, (100, 100, 380, 100))
 
     pygame.display.flip()
     clock.tick(FPS)
