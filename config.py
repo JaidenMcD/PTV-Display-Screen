641370038c6bc4d58773a7c8ee0250ae35618061
JaidenMcD <jmcd0014@student.monash.edu>
2025-12-04 11:54:46 +1100

    setup

---
+++ config.py
@@ -0,0 +1,18 @@
+# --- DO NOT CHANGE THESE VALUES ---
+BACKGROUND_COLOR = (0, 0, 0)
+TEXT_COLOR = (255, 255, 255)
+FPS = 1
+
+
+BLACK = (0, 0, 0)
+NETWORK_GREY = (51, 52, 52)
+SANDRINGHAM = (245,126,182)
+FRANKSTON = (0, 150, 57) # werribee and williamston
+CRANBOURNE = (0, 163, 225) # pakenham and cranbourne
+BELGRAVE = (21, 44, 107) #belgrave lilydale alamein glen waverley
+SUNBURY = (249, 190, 22) #craigiburn and upfield
+MERNDA = (200, 16,46) #hurtsbridge and mernda
+SHOWGROUNDS = (149,151,154)
+
+# --- YOU CAN CHANGE THESE VALUES ---
+SCREEN_RES = (480, 320)   # display resolution
---
+++ run.py
@@ -0,0 +1,48 @@
+import pygame, os
+from config import *
+import time 
+from dotenv import load_dotenv
+
+
+load_dotenv()
+print("env file loaded")
+device = int(os.getenv("DEVICE"))
+print(f"device = {device}")
+
+if device == 1:
+    print('setting enviroment variables')
+    os.environ["DISPLAY"] = ":0"
+    os.environ["SDL_FBDEV"] = "/dev/fb1"
+    os.environ["SDL_VIDEODRIVER"] = "fbcon"
+    os.environ["SDL_NOMOUSE"] = "1" 
+
+pygame.init()
+pygame.mouse.set_visible(False)
+
+if device == 1:
+    screen = pygame.display.set_mode(SCREEN_RES, pygame.FULLSCREEN | pygame.NOFRAME)
+else:
+    screen = pygame.display.set_mode(SCREEN_RES)
+
+update_interval = 5 # seconds
+last_update = 0
+
+
+clock = pygame.time.Clock()
+running = True
+
+print('starting up')
+screen.fill(BACKGROUND_COLOR)
+time.sleep(1)
+screen.fill(TEXT_COLOR)
+time.sleep(1)
+screen.fill(BACKGROUND_COLOR)
+
+
+while running:
+    screen.fill(BACKGROUND_COLOR)
+
+    pygame.display.flip()
+    clock.tick(FPS)
+
+pygame.quit()
