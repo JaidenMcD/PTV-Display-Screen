1a51c9cf34876d73447e793cbdc30addeb30dd79
JaidenMcD <jmcd0014@student.monash.edu>
2025-12-04 13:22:23 +1100

    Update run.py

--- run.py
+++ run.py
@@ -22,7 +22,7 @@ pygame.init()
 pygame.mouse.set_visible(False)
 
 if device == 1:
-    screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)  
+    screen = pygame.display.set_mode(SCREEN_RES)
 else:
     screen = pygame.display.set_mode(SCREEN_RES)
 
