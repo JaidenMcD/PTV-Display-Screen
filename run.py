eb177293e394bc40515a29a8b52f2c9e77ce43b6
JaidenMcD <jmcd0014@student.monash.edu>
2025-12-04 12:08:19 +1100

    Update run.py

--- run.py
+++ run.py
@@ -11,7 +11,7 @@ print(f"device = {device}")
 
 if device == 1:
     print('setting enviroment variables')
-    os.environ["SDL_FBDEV"] = "/dev/fb1"
+    os.environ["SDL_FBDEV"] = "/dev/fb0"
     os.environ["SDL_VIDEODRIVER"] = "fbcon"
     os.environ["SDL_NOMOUSE"] = "1" 
     os.environ.pop("DISPLAY", None)   # REMOVE X11 DISPLAY
