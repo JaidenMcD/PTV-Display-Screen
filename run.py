5414e30d997919dde26a3e172188cccac3df14bb
JaidenMcD <jmcd0014@student.monash.edu>
2025-12-04 14:40:22 +1100

    Update run.py

--- run.py
+++ run.py
@@ -41,6 +41,7 @@ screen.fill(TEXT_COLOR)
 time.sleep(1)
 screen.fill(BACKGROUND_COLOR)
 
+px12_font = pygame.font.Font('assets/fonts/NETWORKSANS-2019-MEDIUM.TTF', 9)
 
 while running:
     screen.fill(LIGHT_WARM_GREY)
@@ -54,8 +55,29 @@ while running:
     # Time to next train Box
     pygame.draw.rect(screen, BLACK, (379, 15, 91, 33))
 
+    # Top hline
+    pygame.draw.line(screen, BLACK, (11, 70), (SCREEN_RES[0] - 11, 70), 2)
 
-    pygame.draw.rect(screen, SHOWGROUNDS, (100, 100, 380, 100))
+    # Bottom hline
+    pygame.draw.line(screen, BLACK, (11, 213), (353, 213), 2)
+    pygame.draw.line(screen, BLACK, (11, 238), (353, 238), 2)
+
+    # Bottom line indicators
+    pygame.draw.rect(screen, FRANKSTON, (11, 218, 4, 17))
+    pygame.draw.rect(screen, FRANKSTON, (11, 243, 4, 17))
+
+    # Bottom train countdowns
+    pygame.draw.rect(screen, BLACK, (303, 217, 51, 18))
+    pygame.draw.rect(screen, BLACK, (303, 243, 51, 18))
+    # Bottom train platforms
+    pygame.draw.rect(screen, FRANKSTON, (283, 217, 18, 18))
+    pygame.draw.rect(screen, FRANKSTON, (283, 243, 18, 18))
+
+    # Current time
+    pygame.draw.rect(screen, BLACK, (358, 213, 113, 48))
+    pygame.draw.rect(screen, LIGHT_WARM_GREY, (360, 215, 109, 44))
+    text = px12_font.render("12:42:08 pm", True, BLACK) # Text, antialias, color (white)
+    screen.blit(text, (373, 231)) # Blit at a specific position
 
     pygame.display.flip()
     clock.tick(FPS)
