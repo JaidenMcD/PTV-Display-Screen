0a34cd8758b6886b8db13d1c8b3ad6fc7a68cbf3
JaidenMcD <jmcd0014@student.monash.edu>
2025-12-22 10:11:42 +1100

    uodate

---
+++ app/fonts.py
@@ -0,0 +1,53 @@
+import pygame
+# Fonts
+f_destination_large = pygame.font.Font("assets/fonts/NETWORKSANS-2019-BOLD.TTF", 27)
+f_destination_small = pygame.font.Font("assets/fonts/NETWORKSANS-2019-BOLD.TTF", 14)
+f_depTime_small = pygame.font.Font("assets/fonts/NETWORKSANS-2019-MEDIUM.TTF", 14)
+f_stopsList = pygame.font.Font("assets/fonts/NETWORKSANS-2019-REGULAR.TTF", 12)
+f_bold_10 = pygame.font.Font("assets/fonts/NETWORKSANS-2019-BOLD.TTF", 10)
+f_bold_20 = pygame.font.Font("assets/fonts/NETWORKSANS-2019-BOLD.TTF", 20)
+f_bold_27 = pygame.font.Font("assets/fonts/NETWORKSANS-2019-BOLD.TTF", 27)
+f_bold_25 = pygame.font.Font("assets/fonts/NETWORKSANS-2019-BOLD.TTF", 25)
+
+f_med_11 = pygame.font.Font("assets/fonts/NETWORKSANS-2019-MEDIUM.TTF", 11)
+f_med_12 = pygame.font.Font("assets/fonts/NETWORKSANS-2019-MEDIUM.TTF", 12)
+f_med_14 = pygame.font.Font("assets/fonts/NETWORKSANS-2019-MEDIUM.TTF", 14)
+f_med_17 = pygame.font.Font("assets/fonts/NETWORKSANS-2019-MEDIUM.TTF", 17)
+f_med_22 = pygame.font.Font("assets/fonts/NETWORKSANS-2019-MEDIUM.TTF", 22)
+
+f_reg_9 = pygame.font.Font("assets/fonts/NETWORKSANS-2019-REGULAR.TTF", 9)
+f_reg_10 = pygame.font.Font("assets/fonts/NETWORKSANS-2019-REGULAR.TTF", 10)
+f_reg_12 = pygame.font.Font("assets/fonts/NETWORKSANS-2019-REGULAR.TTF", 12)
+f_reg_13 = pygame.font.Font("assets/fonts/NETWORKSANS-2019-REGULAR.TTF", 13)
+f_reg_15 = pygame.font.Font("assets/fonts/NETWORKSANS-2019-REGULAR.TTF", 15)
+f_reg_17 = pygame.font.Font("assets/fonts/NETWORKSANS-2019-REGULAR.TTF", 17)
+f_reg_21 = pygame.font.Font("assets/fonts/NETWORKSANS-2019-REGULAR.TTF", 21)
+f_reg_23 = pygame.font.Font("assets/fonts/NETWORKSANS-2019-MEDIUM.TTF", 23)
+f_reg_35 = pygame.font.Font("assets/fonts/NETWORKSANS-2019-REGULAR.TTF", 35)
+
+font_list = {
+    "dest_large": f_destination_large,
+    "dest_small": f_destination_small,
+    "dep_small": f_depTime_small,
+    "stops": f_stopsList,
+    "f_bold_10": f_bold_10,
+    "f_bold_20": f_bold_20,
+    "f_bold_27": f_bold_27,
+    "f_bold_25": f_bold_25,
+    
+    "f_med_11": f_med_11,
+    "f_med_12": f_med_12,
+    "f_med_14": f_med_14,
+    "f_med_17": f_med_17,
+    "f_med_22": f_med_22,
+
+    "f_reg_9": f_reg_9,
+    "f_reg_10": f_reg_10,
+    "f_reg_12": f_reg_12,
+    "f_reg_13": f_reg_13,
+    "f_reg_15": f_reg_15,
+    "f_reg_17": f_reg_17,
+    "f_reg_21": f_reg_21,
+    "f_reg_23": f_reg_23,
+    "f_reg_35": f_reg_35
+}
